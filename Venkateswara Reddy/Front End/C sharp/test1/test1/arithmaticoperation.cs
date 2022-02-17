91149f3db016c920d400f1bd4528aa3e596314c7
k Venkateswara Reddy <49361197+kvenkateswarareddy@users.noreply.github.com>
2022-02-17 21:24:43 +0530

    c sharp

---
+++ Venkateswara Reddy/Front End/C sharp/test1/test1/arithmaticoperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program2
+{
+    class Airthmatic
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("enter a and b values to perform arithmatic operations");
+            int a= Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Airthmatic Operations");
+            Console.WriteLine("Addition: " + (a + b));
+            Console.WriteLine("Substraction: " + (a - b));
+            Console.WriteLine("Multiplication: " + (a * b));
+            Console.WriteLine("Division: " + (a / b));
+            Console.WriteLine("Modulus: " + (a % b));
+        }
+    }
+}
