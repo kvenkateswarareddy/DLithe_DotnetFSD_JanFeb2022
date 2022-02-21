c5c8f7ed0c5de7a705ab1d23a853bbb680708461
k Venkateswara Reddy <49361197+kvenkateswarareddy@users.noreply.github.com>
2022-02-21 09:53:50 +0530

    task26

---
+++ Venkateswara Reddy/Front End/C sharp/test2/test2/arraymethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program5
+{
+    class ArrayMethods
+    {
+        static void Display(int[] arr)
+        {
+            foreach (Object ele in arr)
+            {
+                Console.WriteLine(ele + " ");
+            }
+        }
+        static void Main()
+        {
+            int[] vs = { 60, 9, 98, 52, 2};
+            Console.WriteLine("Original Array");
+            foreach (int i in vs)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Sorted Array");
+            Array.Sort(vs);
+            Display(vs);
+            Console.WriteLine("Reverse Array");
+            Array.Reverse(vs);
+            Display(vs);
+        }
+    }
+}
