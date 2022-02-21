903390d3108985c442b1f43fbcdc1d1875a858f9
k Venkateswara Reddy <49361197+kvenkateswarareddy@users.noreply.github.com>
2022-02-21 13:26:21 +0530

    task31

---
+++ Venkateswara Reddy/Front End/C sharp/DLithe_BC_NFS_T_Task31_Debugging/DLithe_BC_NFS_T_Task31_Debugging/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InterfaceDemo
+{
+    interface Iinterface1//added I
+    {
+        public void Test();//added void
+        void Show();
+    }
+    interface Iinterface2
+    {
+        void Test();
+        void Show();
+    }
+    class ImplementInteraface : Iinterface1, Iinterface2// changed : to ,
+    {
+        public void Test()//deleted overide keyword and added void
+        {
+            Console.WriteLine(" Test method is implemented");//added ""
+        }
+        public void Show() ///deleted overide keyword and added void 
+        {
+            Console.WriteLine(" Show method is implemented");//added ""
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImplementInteraface obj = new ImplementInteraface();
+            obj.Test();
+            obj.Show();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
