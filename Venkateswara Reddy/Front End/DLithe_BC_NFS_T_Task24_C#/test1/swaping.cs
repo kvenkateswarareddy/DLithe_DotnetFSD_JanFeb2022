713e6b8e9bf458b521b7e473c7838ea298cb4e14
kvenkateswarareddy <kvenkateswara502@gmail.com>
2022-03-11 17:31:18 +0530

    task 24

---
+++ Venkateswara Reddy/Front End/DLithe_BC_NFS_T_Task24_C#/test1/swaping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class SwapExample
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter a and b values");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Before swap a= " + a + " b= " + b);
+        a = a * b; //a=50 (5*10)      
+        b = a / b; //b=5 (50/10)      
+        a = a / b; //a=10 (50/5)    
+        Console.Write("After swap a= " + a + " b= " + b);
+    }
+}
